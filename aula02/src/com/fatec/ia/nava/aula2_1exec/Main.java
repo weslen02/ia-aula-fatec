d253ad51f1b19abd67f56d8d211fbf487e25b973
Weslen Almeida <weslen-a@hotmail.com>
2020-09-17 14:28:29 -0300

    adding interface, need to work well

--- aula02/src/com/fatec/ia/nava/aula2_1exec/Main.java
+++ aula02/src/com/fatec/ia/nava/aula2_1exec/Main.java
@@ -6,7 +6,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        String path = "<REPLACE>/ia-aula-fatec/aula02/";
+        String path = "/home/weslen/github/ia-aula-fatec/aula02/";
         DataSource arquivo = new DataSource(path + "data/zoo.arff");
         Instances dados = arquivo.getDataSet();
         System.out.println("Instancia lidas " + dados.numInstances());
---
+++ aula02/src/com/fatec/ia/nava/aula2_2exec/ReadData.java
@@ -0,0 +1,24 @@
+package com.fatec.ia.nava.aula2_2exec;
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+
+public class ReadData {
+
+        public void requestWeka() throws Exception {
+
+
+            String path = "/home/weslen/github/ia-aula-fatec/aula02/";
+            DataSource arquivo = new DataSource(path + "data/zoo.arff");
+            Instances dados = arquivo.getDataSet();
+            System.out.println("Instancias lidas: " + dados.numInstances());
+
+            String[] parametros = new String[]{"-R", "1"};
+            Remove filtro = new Remove();
+            filtro.setOptions(parametros);
+            filtro.setInputFormat(dados);
+            dados = Filter.useFilter(dados, filtro);
+        }
+}
